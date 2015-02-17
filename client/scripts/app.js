78e511c8563c9a22ef23a6111f56d7da7447905a
mrblueblue <grokut@gmail.com>
2015-02-17 15:43:48 -0800

    implement room functions

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -8,18 +8,46 @@ var app = function(){
     roomSelect: '#roomSelect',
     defaultUser: 'Anon',
     defaultRoom: 'lobby',
+    currentUser: undefined,
+    currentRoom: 'lobby',
     endpoint: 'https://api.parse.com/1/classes/chatterbox'
   };
 
   app.friends = {
   };
 
+  app.rooms = {
+    lobby: true
+  };
+
   app.currentMessages = {};
 
   app.init = function() {
     $(document).ready(function(){
+      $('#userselect').addClass('show');
       app.addGlobalListeners();
       app.addMessageListeners();
+      app.getRooms();
+    });
+  };
+
+  app.getRooms = function() {
+    this.fetch(app.settings.endpoint, 'order=-createdAt', function(messages) {
+      for ( var i = messages.length - 1; i >= 0; i-- ) {
+        app.rooms[messages[i].roomname] = true;
+      }
+      var selector = $('#roomSelect select');
+      selector.empty();
+      for ( var k in app.rooms ) {
+        var name = Array.prototype.slice.call(k, 0, 20).join('');
+        var option = $('<option></option>');
+        option.text(name).val(name)
+        if ( app.settings.currentRoom === k ) {
+          option.prop('selected', true)
+        }
+        selector.append(option);
+      }
+      selector.append('<option disabled>-----</option><option value="create">Create a Room</option>');
     });
   };
 
@@ -41,20 +69,15 @@ var app = function(){
     });
   };
 
-  app.fetch = function(url, queryString) {
-    queryString = queryString || 'order=-createdAt&limit=5';
+  app.fetch = function(url, queryString, callback) {
+    queryString = queryString || 'where={ "roomname": "' + app.settings.currentRoom + '"}&order=-createdAt';
     $.ajax({
       url: url,
       type: 'GET',
       contentType: 'application/json',
       data: queryString,
       success: function (data) {
-        data.results.forEach(function(message){
-          if ( !app.currentMessages[message.objectId] ){
-            app.currentMessages[message.objectId] = message;
-            app.addMessage(message);
-          }
-        });
+        callback(data.results);
       },
       error: function (data) {
         console.error('chatterbox: Failed to fetch messages');
@@ -63,16 +86,29 @@ var app = function(){
   };
 
   app.addMessage = function(message){
-    var messageBody = $('<div class="message" data-post-id="' + message.objectId + '"><div class="username"></div><div class="message-body"></div><div class="message-room"></div><div class="time">' + message.createdAt + '</div></div>');
+    var messageBody = $('<div class="message" data-post-id="' + message.objectId + '"><div><div class="username"></div><div class="message-room"></div></div><div class="message-body"></div><div class="time">' + moment(message.createdAt).startOf('hour').fromNow() + '</div></div>');
     messageBody.find('.message-body').text(message.text);
     messageBody.find('.username').text(message.username);
-    messageBody.find('.message-room').text(message.roomname);
-    $(app.settings.chatContainer).append(messageBody);
+    messageBody.find('.message-room').text('posted in ' + message.roomname);
+    $(app.settings.chatContainer).prepend(messageBody);
     this.addMessageListeners();
   };
 
   app.clearMessages = function(){
     $(app.settings.chatContainer).empty();
+  };
+
+  app.getNewMessages = function(room){
+    room = room || app.settings.currentRoom;
+    var queryString = 'where={ "roomname": "' + room + '"}&order=-createdAt&limit=20';
+    app.fetch(app.settings.endpoint, queryString, function(messages){
+      for (var i = messages.length - 1; i >= 0; i--){
+        if ( !app.currentMessages[messages[i].objectId] ){
+            app.currentMessages[messages[i].objectId] = messages[i];
+            app.addMessage(messages[i]);
+          }
+      }
+    });
   };
 
   app.addRoom = function(room){
@@ -85,10 +121,11 @@ var app = function(){
 
   app.handleSubmit = function(){
     var message = {};
-    message.username = app.settings.defaultUser;
+    message.username = app.settings.currentUser || app.settings.defaultUser;
     message.text = $('#message textarea').val();
-    message.roomname = app.settings.defaultRoom;
+    message.roomname = app.settings.currentRoom;
     this.send(message);
+    $('#message textarea').val("");
   };
 
   app.addGlobalListeners = function() {
@@ -100,11 +137,31 @@ var app = function(){
     });
 
     $('#fetch').on('click', function(e){
-      app.fetch(app.settings.endpoint);
+      app.getNewMessages();
     });
 
     $('#clear').on('click', function(e){
       app.clearMessages();
+    });
+
+    $('#userselect form').on('submit', function(e) {
+      e.preventDefault();
+      app.settings.currentUser = $(this).find('input').val();
+      $('#userselect.modal-container').removeClass('show');
+    });
+
+    $('#userselect .modal-close').click(function(e){
+      e.preventDefault();
+      $('#userselect.modal-container').removeClass('show');
+    });
+
+    $('#roomSelect select').change(function(){
+      if ( $(this).val() === 'create' ) {
+        $('#roomcreate').addClass('show');
+      } else {
+        var newRoom = $(this).val();
+        app.settings.currentRoom = newRoom;
+      }
     });
 
   };
